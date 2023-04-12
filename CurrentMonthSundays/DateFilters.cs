d5751558cce459533cb30eb0cc41c87f71200118
NazNaheed <75083482+NazNaheed@users.noreply.github.com>
2023-04-12 14:41:11 +0200

    Print Out Current Month Sundays

---
+++ CurrentMonthSundays/DateFilters.cs
@@ -0,0 +1,13 @@
+﻿
+// Saving Current Date
+DateTime today = DateTime.Now;
+// find out the days of month
+int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+// iterate by incrementing the date and fetch only sundays 
+IEnumerable<DateTime> dates = Enumerable.Range(1, daysInMonth)
+                            .Select(n => new DateTime(today.Year, today.Month, n))
+                            .Where(date => date.DayOfWeek == DayOfWeek.Sunday)
+                            .ToArray();
+// print out all the dates of sundays
+foreach (DateTime date in dates)
+{ Console.WriteLine(date); }
